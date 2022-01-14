0021afe53a24909b2b1e0a467ad8f67f0fab9ce1
lijianru <2219269374@qq.com>
2022-01-14 17:41:31 +0800

    add octokit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,17 @@
 import type { NextPage } from 'next';
+import { Octokit } from 'octokit';
+import { useEffect } from 'react';
+
+const octokit = new Octokit();
 
 const Home: NextPage = () => {
+  useEffect(() => {
+    octokit.request('GET /repos/{owner}/{repo}/contributors', {
+      owner: 'octocat',
+      repo: 'hello-world',
+    });
+  }, []);
+
   return <div>test</div>;
 };
 
