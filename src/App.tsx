1cbeb9d9c0992de7c522d6989fba011cd8a5703c
lijianru <2219269374@qq.com>
2022-01-14 18:13:48 +0800

    add antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react';
-import logo from './logo.svg';
 import { Octokit } from 'octokit';
+import { Button } from 'antd';
+import './App.css';
 
 const octokit = new Octokit();
 function App() {
@@ -11,21 +12,8 @@ function App() {
     });
   }, []);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Button>test</Button>
     </div>
   );
 }
